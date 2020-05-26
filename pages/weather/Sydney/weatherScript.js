87951d1fec313dd5acce0c14b95b88b71f9b9a7c
Millan <eaccmk>
2020-05-26 11:38:40 +1000

    Adding Weather as a new Page and changed JS 2 jQuery

--- pages/weather/Sydney/weatherScript.js
+++ pages/weather/Sydney/weatherScript.js
@@ -8,3 +8,19 @@
 			}
 		}
 (document,'script','weatherwidget-io-js');
+
+
+/*
+$(window).resize(function(){
+
+    $('.className').css({
+        position:'absolute',
+        left: ($(window).width() - $('.className').outerWidth())/2,
+        top: ($(window).height() - $('.className').outerHeight())/2
+    });
+
+});
+
+// To initially run the function:
+$(window).resize();
+*/
