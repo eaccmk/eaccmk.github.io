bb9f2ed67d4356218f3546cb9798654bc4d53cf3
Millan <eaccmk>
2020-05-27 21:32:50 +1000

    Added standard footer and Standup Roulette

--- pages/coin/coin.js
+++ pages/coin/coin.js
@@ -1,18 +1,18 @@
-jQuery.noConflict();
-jQuery(document).ready(function($){
-jQuery('#myimg').on('click', function(){
+//jQuery.noConflict();
+$(document).ready(function($){
+$('#myimg').on('click', function(){
 	var flipResult = Math.random();
 	functionOne(flipResult);
 	});
 });
 
 var functionOne = function(flipResult){
-	jQuery('#myimg').removeClass();
-	jQuery('#myimg').text("");
+	$('#myimg').removeClass();
+	$('#myimg').text("");
 	
 	setTimeout(function(){
-		jQuery('#myimg').addClass('spin');
-		jQuery('#lasttime').text(new Date().toLocaleString());
+		$('#myimg').addClass('spin');
+		$('#lasttime').text(new Date().toLocaleString());
 		}, 100);
 		functionTwo(flipResult);
 	}
@@ -21,19 +21,19 @@ var functionTwo = function(flipResult){
 	var quheads = '/assets/coinFlip/headOrange.svg';
 	var qutails = '/assets/coinFlip/tailBlue.svg';
 	
-	var r = jQuery.Deferred();
+	var r = $.Deferred();
 	setTimeout(function(){
 	if(flipResult <= 0.5) {
 			console.log('it is head');
-			jQuery('#result').text('HEADS');
-			jQuery('#result').css('color',`#00aaff`);
-			jQuery('#myimg').attr('src',quheads);		
+			$('#result').text('HEADS');
+			$('#result').css('color',`#00aaff`);
+			$('#myimg').attr('src',quheads);		
 		}
 		else {
 			console.log('it is tail');
-			jQuery('#result').text('TAILS');
-			jQuery('#result').css('color',`#cc0000`);
-			jQuery('#myimg').attr('src',qutails);
+			$('#result').text('TAILS');
+			$('#result').css('color',`#cc0000`);
+			$('#myimg').attr('src',qutails);
 		}
 	}, 3000);
 	return r;
---
+++ pages/tools/roulette/roulette.js
@@ -0,0 +1,116 @@
+//jQuery.noConflict();
+$(document).ready(function($){
+$("#pick").on("click", getRandomName);
+});
+
+var names = new Array(
+    "Names",
+    "Alpha",
+    "Beta",
+    "Cat",
+    "Dog",
+    "Gama",
+    "eaccmk",
+    "Chelsea",
+);
+
+$("#pick").on("click", getRandomName);
+
+var doneNames = new Array()
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomName(){
+  //console.log("clicked!");
+  
+  
+  var namesLength = names.length;
+  console.log("names: ", namesLength);
+  if (namesLength > 0) {
+    $("#pick").html("Go")
+    // var nextNameIdx = getRandomInt(namesLength);
+    // nextName = names[nextNameIdx];
+    //  $("#next").html(nextName)
+     count = 0;                
+    //setTimeout(clearInterval, 2000)
+    flipper = setInterval(flipThrough, 50)
+    
+    
+    console.log("done filtering");
+    return;
+  } 
+  names = doneNames
+  doneNames = new Array()
+  $("#done").html("<ul class=\"single-before\"></ul>");
+  $("#pick").html("Again!")
+  $("#next").html("Done!")
+}
+
+function generateDoneNamesDOM(doneNames){
+  var doneNamesDOM = ""
+  doneNames.forEach(name => {
+    doneNamesDOM = doneNamesDOM + "<li>" + name + "</li>"
+  });
+  //console.log(doneNamesDOM)
+  return doneNamesDOM;
+}
+
+function flipThrough(){
+ 
+  var namesLength = names.length;
+  if (namesLength > 0) {
+    nextNameIdx = getRandomInt(namesLength);
+    nextName = names[nextNameIdx];
+    $("#next").html(nextName);
+    count++;
+    if(count >= 20){
+      clearInterval(flipper)
+      doneNames.push(nextName);
+      console.log("doneNames:", doneNames.length)
+      $("#done ul").html(generateDoneNamesDOM(doneNames));
+      names = names.filter(x => !doneNames.includes(x));
+      return
+    }
+  }
+  
+}
+
+$('#pick.confetti-button').on('click', function(){  
+  function random(max){
+      return Math.random() * (max - 0) + 0;
+  }
+
+  var c = document.createDocumentFragment();
+  for (var i=0; i<100; i++) {
+    var styles = 'transform: translate3d(' + (random(500) - 250) + 'px, ' + (random(200) - 150) + 'px, 0) rotate(' + random(360) + 'deg);\
+                  background: hsla('+random(360)+',100%,50%,1);\
+                  animation: bang 700ms ease-out forwards;\
+                  opacity: 0';
+      
+    var e = document.createElement("i");
+    e.style.cssText = styles.toString();
+    c.appendChild(e);
+}
+// document.body.appendChild(c);
+  $(this).append(c);
+})
+
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
